2a911ccbd212f0027220bff073d3919ab0f7b194
Tyuiu.MotorovaDD@outlook.com <Tyuiu.MotorovaDD@outlook.com>
2023-10-18 18:04:55 +0500

    Task3

---
+++ Tyuiu.MotorovaDD.Sprint2.Task3.V18.Test/DataServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MotorovaDD.Sprint2.Task3.V18.Lib;
+
+namespace Tyuiu.MotorovaDD.Sprint2.Task3.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalculate()
+        {
+            
+            {
+                DataService ds = new DataService();
+                double x = 0;
+                double res = ds.Calculate(x);
+                double wait = 0.75;
+                Assert.AreEqual(wait, res);
+            }
+
+            
+            {
+                DataService ds = new DataService();
+                double x = 1;
+                double res = ds.Calculate(x);
+                double wait = 2;
+                Assert.AreEqual(wait, res);
+            }
+
+           
+            {
+                DataService ds = new DataService();
+                double x = -100;
+                double res = ds.Calculate(x);
+                double wait = -1099.99;
+                Assert.AreEqual(wait, res);
+            }
+        }
+    }
+}
---
+++ Tyuiu.MotorovaDD.Sprint2.Task3.V18/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MotorovaDD.Sprint2.Task3.V18.Lib;
+
+namespace Tyuiu.MotorovaDD.Sprint2.Task3.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнила: Моторова.Д.Д | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнила: Моторова.Д.Д| СМАРТб-23-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение Y с            *");
+            Console.WriteLine("* использование вложенных операторов if - else, где пользователь вводит   *");
+            Console.WriteLine("* значение переменной х с клавиатуры.                                     *");
+            Console.WriteLine("* Округлить до 3 знаков после запятой.                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("* y = x**2(((x+1)/(x-1)))**2 (x>1)                                        *");
+            Console.WriteLine("* y = (x**2-cos(x)**2+10)/(x**2-sin(x)**2+12) (x=0)                       *");
+            Console.WriteLine("* y = (1+1/x**2)**x (-22<x<2)                                             *");
+            Console.WriteLine("* y = x+10x-(1/x) (x<-22)                                                 *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            Console.Write("Введите значение х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение функции: " + ds.Calculate(x));
+
+            Console.ReadKey();
+
+
+
+
+
+}   }   }
